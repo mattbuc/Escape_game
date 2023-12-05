279366cb42a348e87811c82d266e11b124d8480a
Matthias Buczek <matt-buc@orange.fr>
2023-12-05 17:23:58 +0100

    Keypad Working

--- Assets/Script/ButtonVR.cs
+++ Assets/Script/ButtonVR.cs
@@ -13,6 +13,10 @@ public class ButtonVR : MonoBehaviour
     GameObject presser;
     AudioSource sound;
     bool isPressed;
+    float testPush;
+
+    Vector3 OriginalPosButton;
+    Vector3 OriginalPosButtonSign;
 
 
     // Start is called before the first frame update
@@ -20,15 +24,19 @@ public class ButtonVR : MonoBehaviour
     {
         sound = GetComponent<AudioSource>();
         isPressed = false;
+        testPush = -0.12f;
+        OriginalPosButton = button.transform.localPosition;
+        OriginalPosButtonSign = buttonSign.transform.localPosition;
     }
 
     private void OnTriggerEnter(Collider other)
     {
+
         if (!isPressed)
         {
-            button.transform.localPosition = new Vector3(0, 0, -0.09f);
-            buttonSign.transform.localPosition = new Vector3(1.950179f, -0.02342248f, -0.12f);
-            presser = other.gameObject;
+            button.transform.localPosition = new Vector3(OriginalPosButton.x, OriginalPosButton.y, -0.09f);
+            buttonSign.transform.localPosition = new Vector3(OriginalPosButtonSign.x, OriginalPosButtonSign.y, testPush);
+            presser = other.gameObject; 
             onPress.Invoke();
             sound.Play();
             isPressed = true;
@@ -40,10 +48,11 @@ public class ButtonVR : MonoBehaviour
     {
         if (isPressed)
         {
-            button.transform.localPosition = new Vector3(0, 0, 0);
-            buttonSign.transform.localPosition = new Vector3(1.950179f, -0.02342248f, -0.03257556f);
+            button.transform.localPosition = new Vector3(OriginalPosButton.x, OriginalPosButton.y, OriginalPosButton.z);
+            buttonSign.transform.localPosition = new Vector3(OriginalPosButtonSign.x, OriginalPosButtonSign.y, OriginalPosButtonSign.z);
             onRelease.Invoke();
             isPressed = false;
+            Debug.Log("Button UnPressed");
         }
     }
 
@@ -51,4 +60,5 @@ public class ButtonVR : MonoBehaviour
     {
         Debug.Log("Button DePressed");
     }
+
 }
